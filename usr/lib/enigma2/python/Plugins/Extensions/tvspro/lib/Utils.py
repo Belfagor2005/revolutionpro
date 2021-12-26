271d1cdbfccc75f63a29233a5cc4d775551edfdd
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2021-12-26 19:35:31 +0100

    as fixed

--- usr/lib/enigma2/python/Plugins/Extensions/tvspro/lib/Utils.py
+++ usr/lib/enigma2/python/Plugins/Extensions/tvspro/lib/Utils.py
@@ -14,21 +14,24 @@ import base64
 PY3 = sys.version_info.major >= 3
 if PY3:
     # Python 3
-    str = unicode = basestring = str
-    unichr = chr; long = int
+    PY3 = True
+    unicode = str; unichr = chr; long = int; xrange = range
     from urllib.parse import quote
     from urllib.request import urlopen
     from urllib.request import Request
+    from urllib.error import HTTPError, URLError
+    
 else:
-    # Python 2
-    _str = str
-    str = unicode
-    range = xrange
-    unicode = unicode
-    basestring = basestring
+    # # Python 2
+    # _str = str
+    # str = unicode
+    # range = xrange
+    # unicode = unicode
+    # basestring = basestring
     from urllib import quote
     from urllib2 import urlopen
     from urllib2 import Request
+    from urllib2 import HTTPError, URLError
 
 def getDesktopSize():
     from enigma import getDesktop
@@ -57,6 +60,13 @@ def mySkin():
     currentSkin = config.skin.primary_skin.value.replace('/skin.xml', '')
     return currentSkin
 
+if os.path.exists('/usr/lib/enigma2/python/Plugins/Extensions/MediaPlayer'):
+    from Plugins.Extensions.MediaPlayer import *
+    MediaPlayerInstalled = True
+else:
+    MediaPlayerInstalled = False
+
+
 def listDir(what):
     f = None
     try:
@@ -65,6 +75,15 @@ def listDir(what):
         pass
 
     return f
+    
+def remove_line(filename, what):
+    if os.path.isfile(filename):
+        file_read = open(filename).readlines()
+        file_write = open(filename, 'w')
+        for line in file_read:
+            if what not in line:
+                file_write.write(line)
+        file_write.close()
 
 def getLanguage():
     try:
@@ -116,6 +135,11 @@ def checkInternet():
         return False
 
 def check(url):
+    import socket
+    try:
+        from urllib.error import HTTPError, URLError
+    except:
+        from urllib2 import HTTPError, URLError
     try:
         response = checkStr(urlopen(url, None, 5))
         response.close()
@@ -126,6 +150,15 @@ def check(url):
         return False
     except socket.timeout:
         return False
+        
+def testWebConnection(host="www.google.com", port=80, timeout=3):
+	import socket
+	try:
+		socket.setdefaulttimeout(timeout)
+		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+		return True
+	except Exception as ex:
+		return False
 
 def checkStr(txt):
     # convert variable to type str both in Python 2 and 3
@@ -162,19 +195,42 @@ def freespace():
         return ''
 
 def b64encoder(source):
+    import base64
     if PY3:
         source = source.encode('utf-8')
     content = base64.b64encode(source).decode('utf-8')
     return content
 
-def b64decoder(source):
-    if PY3:
-        # source = source.decode('utf-8')
-        import base64
-        source = base64.b64decode(source).decode('utf-8')
-    content = source
-    return content
-
+   
+def b64decoder(s):
+    """Add missing padding to string and return the decoded base64 string."""
+    import base64
+    s = str(s).strip()
+    try:
+        # return base64.b64decode(s)
+        outp = base64.b64decode(s)
+        print('outp1 ', outp)
+        if PY3:   
+            outp = outp.decode('utf-8')
+            print('outp2 ', outp)
+        return outp
+        
+    except TypeError:
+        padding = len(s) % 4
+        if padding == 1:
+            print("Invalid base64 string: {}".format(s))
+            return ''
+        elif padding == 2:
+            s += b'=='
+        elif padding == 3:
+            s += b'='
+        outp = base64.b64decode(s)
+        print('outp1 ', outp)
+        if PY3:   
+            outp = outp.decode('utf-8')
+            print('outp2 ', outp)
+        return outp
+        
 def __createdir(list):
     dir = ''
     for line in list[1:].split('/'):
@@ -196,7 +252,6 @@ try:
 	is_imdb = True
 except Exception:
 	is_imdb = False
-
 
 def substr(data,start,end):
     i1 = data.find(start)
@@ -359,7 +414,24 @@ def isStreamlinkAvailable():
         # return urlopen(url, context=sslContext)
     # else:
         # return urlopen(url)
-
+def AdultUrl(url):
+        if sys.version_info.major == 3:
+             import urllib.request as urllib2
+        elif sys.version_info.major == 2:
+             import urllib2
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+        r = urllib2.urlopen(req, None, 15)
+        link = r.read()
+        r.close()
+        tlink = link
+        if str(type(tlink)).find('bytes') != -1:
+            try:
+                tlink = tlink.decode("utf-8")
+            except Exception as e:
+                   print("Error: %s." % e)
+        return tlink
+        
 from random import choice
 ListAgent = [
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
@@ -421,6 +493,24 @@ ListAgent = [
 def RequestAgent():
 	RandomAgent = choice(ListAgent)
 	return RandomAgent
+
+def ReadUrl2(url):
+    if sys.version_info.major == 3:
+         import urllib.request as urllib2
+    elif sys.version_info.major == 2:
+         import urllib2
+    req = urllib2.Request(url)                      
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
+    r = urllib2.urlopen(req, None, 15)
+    link = r.read()
+    r.close()
+    content = link
+    if str(type(content)).find('bytes') != -1:
+        try:
+            content = content.decode("utf-8")                
+        except Exception as e:                   
+               print("Error: %s." % e)   
+    return content
 
 def ReadUrl(url):
     if sys.version_info.major == 3:
