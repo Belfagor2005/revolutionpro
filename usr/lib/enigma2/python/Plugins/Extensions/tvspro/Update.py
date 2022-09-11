484569cda263c711e42d71fb1907a8a9a56f238c
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2022-09-11 11:30:18 +0200

    utils

--- usr/lib/enigma2/python/Plugins/Extensions/tvspro/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/tvspro/Update.py
@@ -3,7 +3,6 @@ from twisted.web.client import downloadPage
 PY3 = sys.version_info.major >= 3
 print("Update.py")
 def upd_done():        
-    from twisted.web.client import downloadPage
     print( "In upd_done")
     xfile ='http://patbuweb.com/revolutionlite/tvspro.tar'
     print('xfile: ', xfile)
